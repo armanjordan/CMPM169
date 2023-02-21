0240491feb3c6ff0177d9d6a136a9a60f955446f
Arman Jordan <arajorda@ucsc.edu>
2023-02-21 03:23:20 -0800

    added 6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,37 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+'use strict';
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var font = 'sans-serif';
+var letter = 'I';
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(0);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  textFont(font);
+  textAlign(CENTER, CENTER);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+function mouseMoved() {
+  clear();
+  textSize(sqrt((mouseX - width / 2) * (mouseX - width / 2)));
+  text(letter, windowWidth - mouseX, mouseY);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseDragged() {
+  textSize(sqrt((mouseY - height / 2) * (mouseY - height / 2)));
+  text(letter, windowWidth - mouseX, mouseY);
+}
+
+function keyTyped() {
+  letter += key;
+  if (letter.length % 11 == 0 && letter.length < 54) {
+      letter = letter += "\n";
+  }
+  if (letter.length >= 55) {
+    letter = letter.substring(11, 56);
+    letter = letter += "\n";
+  }
+  clear();
+  text(letter, windowWidth - mouseX, mouseY);
 }
